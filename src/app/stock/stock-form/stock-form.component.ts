a104a5ba563b32cefb9588eec07a3eb5f6295470
neilyoTracer <kk_cyndi@126.com>
2017-09-23 22:51:44 +0800

    the ngForm

--- src/app/stock/stock-form/stock-form.component.ts
+++ src/app/stock/stock-form/stock-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Stock, StockService } from '../stock.service';
 import { ActivatedRoute,Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, FormControl, FormArray } from "@angular/forms";
 
 @Component({
   selector: 'app-stock-form',
@@ -9,7 +10,11 @@ import { ActivatedRoute,Router } from '@angular/router';
 })
 export class StockFormComponent implements OnInit {
 
+  formModel:FormGroup;
+
   stock:Stock;
+
+  categories = ["IT","互联网","金融"];
 
   constructor(
     private routeInfo:ActivatedRoute,
@@ -21,6 +26,28 @@ export class StockFormComponent implements OnInit {
     let stockId = this.routeInfo.snapshot.params["id"];
     // console.log(stockId);
     this.stock = this.stockService.getStock(stockId);
+
+    let fb = new FormBuilder();
+    this.formModel = fb.group(
+      {
+        name:[this.stock.name,[Validators.required,Validators.minLength(3)]],
+        price:[this.stock.price,Validators.required],
+        desc:[this.stock.desc],
+        categories:fb.array([
+          new FormControl(this.stock.categories.indexOf(this.categories[0]) != -1),
+          new FormControl(this.stock.categories.indexOf(this.categories[1]) != -1),
+          new FormControl(this.stock.categories. indexOf(this.categories[2]) != -1)
+        ],this.categoriesCheckValidator)
+      }
+    )
+  }
+
+  categoriesCheckValidator(control:FormArray) {
+    let valid:boolean;
+    valid = control.controls.some((item) => {
+      return item.value;
+    })
+    return valid ? null : {categoriesLength:true};
   }
 
   cancel(){ 
@@ -28,7 +55,17 @@ export class StockFormComponent implements OnInit {
   }
 
   save() { 
-    this.router.navigateByUrl('/stock');
+    let chineseCategories = [];
+    let index = 0;
+    for(let i=0;i<3;i++) { 
+      if(this.formModel.value.categories[i]) { 
+        chineseCategories[index++] = this.categories[i];
+      }
+    }
+    this.formModel.value.categories = chineseCategories;
+    this.formModel.value.rating = this.stock.rating;
+    console.log(this.formModel.value);
+    // this.router.navigateByUrl('/stock');
   }
 
 }
